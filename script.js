18f2459318fc8157dd61f802b768ddf652307c3f
EduardLopatin <blackbars48@gmail.com>
2016-11-04 18:31:12 +0200

    need refactoring

--- script.js
+++ script.js
@@ -5,12 +5,69 @@
         console.log(usersData);
         createUserListField();
         createUsers(usersData);
+        createUserInfoField();
 
         function createUsers(usersData) {
             var target = document.getElementById('userList');
             usersData.forEach(function (user) {
-                createListItem(user, target)
+                createListItem(user, target);
             })
+        }
+        function createUserInfoField() {
+            createInfoBlock();
+            createInfoForm();
+         }
+        function createInfoForm() {
+            var infoForm = createDomElementsForInfoForm();
+            putInfoFormDomElementsOnPlaces(infoForm);
+            setStyleForInfoForm(infoForm);
+            setIdForInfoFormElements(infoForm);
+        }
+        function setIdForInfoFormElements(infoForm) {
+            infoForm.topic.id = 'topic'
+            infoForm.addressBlock.id = 'address'
+        }
+        function setStyleForInfoForm(infoForm) {
+            var topicStyle, addressStyle, mainInfoStyle
+            topicStyle= infoForm.topic.style;
+            topicStyle.textAlign = 'right';
+            topicStyle.marginRight = '20px';
+
+            addressStyle = infoForm.addressBlock.style
+            addressStyle.textAlign = 'right';
+            addressStyle.marginRight = '20px';
+
+
+        }
+        function putInfoFormDomElementsOnPlaces(infoForm) {
+            infoForm.target.appendChild(infoForm.topic);
+            infoForm.target.appendChild(infoForm.addressBlock);
+            infoForm.target.appendChild(infoForm.mainInfoBlock);
+        }
+        function createDomElementsForInfoForm() {
+           return {
+                topic: document.createElement('h1'),
+                addressBlock: document.createElement('div'),
+                mainInfoBlock: document.createElement('div'),
+                target: document.getElementById('infoField')
+            };
+        }
+        function createInfoBlock() {
+             var infoField = document.createElement('div');
+             infoField.id = 'infoField';
+             infoField.style.width = '60vw';
+             infoField.style.float = 'left';
+             infoField.style.marginLeft = '20px';
+             infoField.style.backgroundColor = 'lightgray';
+             document.body.appendChild(infoField);
+         }
+        function declareAction(block) {
+            block.onclick = action
+        }
+        function action() {
+            var user
+            user = usersData[this.id];
+            console.log(user);
         }
         function createListItem(user, target) {
             var newUser = {};
@@ -18,6 +75,11 @@
             setStyleForItem(newUser);
             putDomElementsOnPlaces(newUser, target);
             setDataInItem(newUser , user);
+            declareAction(newUser.block);
+            setId(user, newUser.block)
+        }
+        function setId(user, block) {
+           block.id =  usersData.indexOf(user);
         }
         function setDataInItem(newUser, userData){
             newUser.avatar.setAttribute('src', userData.general.avatar)
@@ -62,6 +124,7 @@
         function createUserListField() {
             var userListField = document.createElement('div');
             userListField.style.width = '17vw';
+            userListField.style.float = 'left';
             userListField.id = 'userList';
             document.body.appendChild(userListField);
         }
